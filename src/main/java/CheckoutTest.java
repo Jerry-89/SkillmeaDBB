e31cd6bd2d129300aada9abc368714f0bfbf62c1
Jerry-89 <kontakt.jerry@gmail.com>
2025-06-19 21:25:09 +0200

    comit 2

--- src/main/java/CheckoutTest.java
+++ src/main/java/CheckoutTest.java
@@ -1,3 +1,5 @@
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -5,11 +7,16 @@ import product.Checkout;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+
 public class CheckoutTest {
     Checkout myCheckout;
+    @Before
+    public void totoSaSpustiPredKazdymTestom (){
+        myCheckout = new Checkout();
+    }
     @Given("the price of a {string} is {int} eur")
     public void thePriceOfAIsEur(String name, int price) {
-        myCheckout = new Checkout();
+
         myCheckout.addItem(name, price);
     }
 
@@ -21,5 +28,17 @@ public class CheckoutTest {
     @Then("Total price should be {int}")
     public void totalPriceShouldBe(int expectedTotalPrice) {
         assertEquals(expectedTotalPrice, myCheckout.totalCart());
+    }
+
+    @And("price of {string} is {int} eur")
+    public void priceOfIsEur(String arg0, int arg1) {
+    }
+
+    @And("user checkout {string} {int}")
+    public void userCheckout(String arg0, int arg1) {
+    }
+
+    @Then("total price should be {int} eur")
+    public void totalPriceShouldBeEur(int arg0) {
     }
 }
--- src/main/resources/Checkout.feature
+++ src/main/resources/Checkout.feature
@@ -6,3 +6,14 @@ Feature: Checkout
     #ak je slovo v "" tak sa zmeni na premennu
     When User checkout "banana" 2
     Then Total price should be 10
+
+  Scenario Outline: Checkout banana and apple
+    Given the price of a "banana" is 2 eur
+    And price of "apple" is 1 eur
+    When User checkout "banana" 2
+    And user checkout "apple" 2
+    Then total price should be 6 eur
+
+    Examples:
+    | bananaPrice | applePrice | total | appleCount |
+    | 80          | 140        |  160  | 5          |
