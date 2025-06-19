967526398f0f56a93f5004bffcf46b052b6c0f04
Jerry-89 <kontakt.jerry@gmail.com>
2025-06-19 20:32:58 +0200

    comit 1

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,22 +1,50 @@
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import product.BankAccount;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTest {
 BankAccount firstAccount;
+BankAccount secondAccount;
 
 
     @Given("Bank account with {int} eur")
     public void bankAccountWithEur(int initialBalance) {  //arg0 treba prepisat na premennu
       firstAccount = new BankAccount(initialBalance);
-
-
+      secondAccount = new BankAccount(initialBalance);
     }
 
     @Then("A user account balance is {int} eur")
     public void aUserAccountBalanceIsEur(int expectedBalance) {
+    assertEquals(expectedBalance, firstAccount.getAccountBalance());
 
+    }
 
+    @When("User withdraws {int} eur")
+    public void userWithdrawsEur(int withdrawAmount) {
+        firstAccount.withdraw(withdrawAmount);
+    }
+
+    @And("User transfer {int} eur to this account")
+    public void userTransferEurToThisAccount(int transferAmount) {
+        firstAccount.transfer(transferAmount);
+    }
+
+    @And("Another Bank account with {int} eur")
+    public void anotherBankAccountWithEur(int initialBalance) {
+        secondAccount = new BankAccount(initialBalance);
+    }
+
+    @When("User transfer from first one to second one {int} eur")
+    public void userTransferFromFirstOneToSecondOneEur(int transferAmount) {
+        firstAccount.withdraw(transferAmount);
+        secondAccount.transfer(transferAmount);
+    }
+
+    @And("A user second account balance is {int} eur")
+    public void aUserSecondAccountBalanceIsEur(int expectedBalance) {
+        assertEquals(expectedBalance, secondAccount.getAccountBalance());
     }
 }
--- src/main/resources/Bank.feature
+++ src/main/resources/Bank.feature
@@ -4,3 +4,30 @@
     Scenario: Bank account balance
       Given Bank account with 1000 eur
       Then  A user account balance is 1000 eur
+
+    Scenario: Bank account transfer
+      Given Bank account with 500 eur
+      When User withdraws 250 eur
+      And User transfer 1250 eur to this account
+      Then A user account balance is 1500 eur
+
+    Scenario Outline: Bank account transfer
+      Given Bank account with <initBalance> eur
+      When User withdraws <transferAmount> eur
+      And User transfer <withdrawAmount> eur to this account
+      Then A user account balance is <expectedBalance> eur
+      #opat viacero hodnot, treba napisat scenario outline, | sa pise na AJ klavensici Shift + klaves pri prehlasovanom A
+      Examples:
+      | initBalance | withdrawAmount | transferAmount | expectedBalance |
+      | 500 | 250 | 1250 | 1500 |
+      | 500 | 100 | 1000 | 1400 |
+      | -500 | 0 | 600 | -500 |
+
+      ###########################
+
+  Scenario: Multiple Bank Account Transfer
+    Given Bank account with 1000 eur
+    And Another Bank account with 2000 eur
+    When User transfer from first one to second one 500 eur
+    Then A user account balance is 500 eur
+    And A user second account balance is 2500 eur
