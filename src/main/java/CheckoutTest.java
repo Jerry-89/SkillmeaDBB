1dbee48b8a110cfbcddb63a63f1dc09094f5f30d
Jerry-89 <kontakt.jerry@gmail.com>
2025-06-19 21:01:09 +0200

    comit 2

--- src/main/java/CheckoutTest.java
+++ src/main/java/CheckoutTest.java
@@ -1,17 +1,25 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import product.Checkout;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CheckoutTest {
+    Checkout myCheckout;
     @Given("the price of a {string} is {int} eur")
-    public void thePriceOfAIsEur(String arg0, int arg1) {
+    public void thePriceOfAIsEur(String name, int price) {
+        myCheckout = new Checkout();
+        myCheckout.addItem(name, price);
     }
 
     @When("User checkout {string} {int}")
-    public void userCheckout(String arg0, int arg1) {
+    public void userCheckout(String name, int count) {
+        myCheckout.scanItems(name, count);
     }
 
     @Then("Total price should be {int}")
-    public void totalPriceShouldBe(int arg0) {
+    public void totalPriceShouldBe(int expectedTotalPrice) {
+        assertEquals(expectedTotalPrice, myCheckout.totalCart());
     }
 }
